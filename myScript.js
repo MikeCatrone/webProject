a37fa3c47272c156d76d8014740a3308df7d1a36
Mike Catrone <mike_catrone@yahoo.com>
2026-03-27 16:52:12 -0300

    Updated logic

--- myScript.js
+++ myScript.js
@@ -1,110 +1,141 @@
 
-// This will determine which "page" to load. Product is the default/home page
-let page = "product";
-
-
 
 
 $(document).ready(() => {
 
+
+    // Slider code
+    const initSlider = () => {
+
+            $('.slider').sss({
+            slideShow : true,
+            startOn   : 0,
+            transition: 400,
+            speed     : 3500,
+            showNav   : true
+        });
+        
+    }
+   
     
-    if(page === "product") {
 
-        $("#myMain").html(`
-            <div>
-                <h2>Welcome to my eShop guys!</h2>
+    // Slider and welcome message html
+    let theSlider = `
+        <div id="homeBanner">
+            <h2>Mike's Retro Chest</h2>
 
-                <div class="slider">
-                    <img src="./myImages/slider01.png" />
-                    <img src="./myImages/slider03.png" />
-                    <img src="./myImages/slider02.png" />
-                    <img src="./myImages/slider04.png" />
-                </div>
+            <div class="slider">
+                <img id='imageTest' src="./myImages/slider01.png" />
+                <img src="./myImages/slider03.png" />
+                <img src="./myImages/slider02.png" />
+                <img src="./myImages/slider04.png" />
+            </div>
+        </div>
+        
+    `;
 
-                <h2>Games For Sale</h2>
 
-                <div class="container mt-5">
-                    <div class="row g-4">
-                        
-                        <div class="col-md-4">
-                            <div class="p-3 border bg-light text-center">
-                                <img src="./myImages/01.jpg">
-                            </div>
-                        </div>
-                        
-                        <div class="col-md-4">
-                            <div class="p-3 border bg-light text-center">
-                                <img src="./myImages/01.jpg">
-                            </div>
-                        </div>
-                        
-                        <div class="col-md-4">
-                            <div class="p-3 border bg-light text-center">
-                                <img src="./myImages/01.jpg">
-                            </div>
-                        </div>
+    // Page content html
+    let pageContent = `
+        <div id = "pageContent">
+            
+            <h2>Games For Sale</h2>
 
-                        <div class="col-md-4">
-                            <div class="p-3 border bg-light text-center">
-                                <img src="./myImages/01.jpg">
-                            </div>
+            <div class="container mt-5">
+                <div class="row g-4">
+                    
+                    <div class="col-md-4">
+                        <div class="p-3 border bg-light text-center">
+                            <img src="./myImages/01.jpg">
                         </div>
-                        
-                        <div class="col-md-4">
-                            <div class="p-3 border bg-light text-center">
-                                <img src="./myImages/01.jpg">
-                            </div>
+                    </div>
+                    
+                    <div class="col-md-4">
+                        <div class="p-3 border bg-light text-center">
+                            <img src="./myImages/01.jpg">
                         </div>
-                        
-                        <div class="col-md-4">
-                            <div class="p-3 border bg-light text-center">
-                                <img src="./myImages/01.jpg">
-                            </div>
+                    </div>
+                    
+                    <div class="col-md-4">
+                        <div class="p-3 border bg-light text-center">
+                            <img src="./myImages/01.jpg">
                         </div>
-                        
                     </div>
 
-            </div>
+                    <div class="col-md-4">
+                        <div class="p-3 border bg-light text-center">
+                            <img src="./myImages/01.jpg">
+                        </div>
+                    </div>
+                    
+                    <div class="col-md-4">
+                        <div class="p-3 border bg-light text-center">
+                            <img src="./myImages/01.jpg">
+                        </div>
+                    </div>
+                    
+                    <div class="col-md-4">
+                        <div class="p-3 border bg-light text-center">
+                            <img src="./myImages/01.jpg">
+                        </div>
+                    </div>
+                    
+                </div>
 
-        `);
+        </div>
 
-
-        // Slider Setup //
-        $('.slider').sss({
-            slideShow : true,    // Set to false to prevent SSS from automatically animating.
-            startOn   : 0,       // Slide to display first. Uses array notation (0 = first slide).
-            transition: 400,     // Length (in milliseconds) of the fade transition.
-            speed     : 3500,    // Slideshow speed in milliseconds.
-            showNav   : true     // Set to false to hide navigation arrows.
-        });
+    `;
 
 
 
-    } else if(page === "about") {
+    //Initial Home Page Load
+    $(() => {
 
-        $("#myMain").html(`
+        console.log("Initial Load");
+        
+        $("#myMain").append(theSlider);
+        $("#myMain").append(pageContent);
+
+        // Activates slider
+        initSlider();
+
+
+    })
+
+
+
+    // Home Button handler
+    $("#home").on('click', () => {
+
+        $("#homeBanner").css("display", "block");
+        $("#pageContent").html(pageContent);
+        
+    })
+
+
+
+    // About Button handler
+    $("#about").on('click', () => {
+
+        $("#homeBanner").css("display", "none");
+        $("#pageContent").html(`
             <h1>About Us</h1>
-        `);
+        `)
 
-
-    } else if(page === "contact") {
-
-        $("#myMain").html(`
-            <h1>Contact Us</h1>
-        `);
+    })
 
 
 
-    } else {
+    // Contact Button handler
+    $("#contact").on('click', () => {
 
-        $("#myMain").html(`
-            <h1>Something went wrong :(</h1>
+        $("#homeBanner").css("display", "none");
+        $("#pageContent").html(`
+            <h1>Contact</h1>
+        `)
 
-        `);
+    })
 
-
-    }
-    
 
 })
 
@@ -115,24 +146,19 @@ $(document).ready(() => {
 // Modal //
 
 // This code is from the developer's code pen demo
-document.addEventListener('DOMContentLoaded', () => {
-    MicroModal.init({
-        onClose: () => { console.log('close') }
-    })
+// document.addEventListener('DOMContentLoaded', () => {
+//     MicroModal.init({
+//         onClose: () => { console.log('close') }
+//     })
 
-    MicroModal.show('modal-1')
-    MicroModal.close('modal-1')
+//     MicroModal.show('modal-1')
+//     MicroModal.close('modal-1')
 
-});
+// });
 
 /////////////////////////////////////////////////////////////
 
 
-
-$(() => {
-    console.log("Helloss");
-    
-})
 
 
 
